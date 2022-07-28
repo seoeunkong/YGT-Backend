ca4b16de6b6c697770d85ab96b04ac49c15c8c43
seoeunkong <seoeun7872@gmail.com>
2022-07-28 11:22:36 +0900

    post model commit

--- postapp/models.py
+++ postapp/models.py
@@ -1,3 +1,10 @@
+from operator import mod
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+#게시물 모델
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
